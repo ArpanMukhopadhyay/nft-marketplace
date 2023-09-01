7eca9b5dbb8b1c86c2362efbc14846fe830720c7
ArpanMukhopadhyay <amukhopadhyay02@gmail.com>
2023-09-01 14:00:23 +0930

    dynamic_images

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import {useEffect, useState} from 'react';
+import axios from "axios";
 
 const HotCollections = () => {
+  const [users, setUsers] = useState([]);
+  async function main(){
+    const {data} = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+    setUsers(data) 
+  }
+  useEffect(()=> {
+    main()
+  },[])
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,25 +23,25 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {new Array(6).fill(0).map((_, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={users[index]?.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to={`/item-details/${users[index]?.nftId}`}>
+                    <img src={users[index]?.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={users[index]?.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{users[index]?.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{users[index]?.code}</span>
                 </div>
               </div>
             </div>
